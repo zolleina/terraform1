acccbd210d3365d272c79dc74bf1ee91368ed37f
Aniello Zanfardino <azanfard@cisco.com>
2024-01-16 14:44:56 +0100

    Commit 3 with lock

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "terraform-backend-1"
     key    = "network/terraform.tfstate"
     region = "eu-north-1"
+    dynamodb_table = "terraform1-state-locking"
   }
 }
--- vpc1.tf
+++ vpc1.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "VPC_1_terraform_test" {
+resource "aws_vpc" "VPC_1_terraform_test_1" {
   cidr_block       = "10.1.0.0/16"
 
   tags = {
-    Name = "VPC_1_Terraform_test"
+    Name = "VPC_1_Terraform_test_1"
   }
 }
