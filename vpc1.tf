7c6fc72beec26383b6f2b498da02bd48ab105927
zolleina <38134995+zolleina@users.noreply.github.com>
2024-01-17 11:16:42 +0100

    changed vpc name

--- vpc1.tf
+++ vpc1.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region     = "eu-north-1"
 }
 
-resource "aws_vpc" "VPC_1_terraform_test_4" {
+resource "aws_vpc" "VPC_1_terraform" {
   cidr_block       = "10.1.0.0/16"
 
   tags = {
-    Name = "VPC_1_Terraform_test_4"
+    Name = "VPC_1_Terraform_test_5"
   }
 }
